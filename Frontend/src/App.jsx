3bb205343000c9864079d01c927ad5751e454154
Lokeshwar R <lokeshwarrajan4@gmail.com>
2025-08-17 00:51:34 +0530

    Organized files

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React Frontend</h1>
+      <p>This is a standalone frontend app 🚀</p>
+    </div>
+  );
+}
+
+export default App;
