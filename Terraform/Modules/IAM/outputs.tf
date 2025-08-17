2d8ea7c8581f14f652674f3cc2faa7f0501b8f0a
Lokeshwar R <lokeshwarrajan4@gmail.com>
2025-08-18 02:48:36 +0530

    Update outputs.tf

--- Terraform/Modules/IAM/outputs.tf
+++ Terraform/Modules/IAM/outputs.tf
@@ -1,3 +1,7 @@
 output "role_arn" {
   value = aws_iam_role.this.arn
 }
+output "instance_profile_name" {
+  value       = try(aws_iam_instance_profile.this[0].name, null)
+  description = "Instance profile name if created, else null"
+}
