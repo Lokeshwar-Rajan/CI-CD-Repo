fa70b3e90a5328dbbe5189a0a2b17259682c747a
Lokeshwar R <lokeshwarrajan4@gmail.com>
2025-08-18 02:48:09 +0530

    Update main.tf

--- Terraform/Modules/IAM/main.tf
+++ Terraform/Modules/IAM/main.tf
@@ -18,3 +18,9 @@ resource "aws_iam_role_policy_attachment" "attachments" {
 role = aws_iam_role.this.name
   policy_arn = each.value
 }
+resource "aws_iam_instance_profile" "this" {
+  count = var.create_instance_profile ? 1 : 0
+
+  name = "${var.role_name}-profile"
+  role = aws_iam_role.this.name
+}
