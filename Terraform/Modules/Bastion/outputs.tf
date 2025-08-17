7f7a1ba4def0421265828572b8d5a4d67e3e98a7
Lokeshwar R <lokeshwarrajan4@gmail.com>
2025-08-18 02:24:09 +0530

    Update outputs.tf

--- Terraform/Modules/Bastion/outputs.tf
+++ Terraform/Modules/Bastion/outputs.tf
@@ -1,11 +1,11 @@
 output "bastion_instance_id" {
-  value       = aws_instance.bastion.id
+  value       = aws_instance.bastion1.id
 }
 
 output "bastion_public_ip" {
-  value       = aws_instance.bastion.public_ip
+  value       = aws_instance.bastion1.public_ip
 }
 
 output "bastion_private_ip" {
-  value       = aws_instance.bastion.private_ip
+  value       = aws_instance.bastion1.private_ip
 }
