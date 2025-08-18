e88775af3db9a42f2e73b203fcabc65d7dbdd5a8
Lokeshwar R <lokeshwarrajan4@gmail.com>
2025-08-19 02:39:29 +0530

    Update variables.tf

--- Terraform/Modules/ALB/variables.tf
+++ Terraform/Modules/ALB/variables.tf
@@ -19,35 +19,35 @@ variable "certificate_arn" {
 
 }
 
-# variable "frontend_health_check_path" {
-#   type        = string
-#   default     = "/"
-# }
+variable "frontend_health_check_path" {
+   type        = string
+   default     = "/"
+ }
 
-# variable "frontend_health_check_matcher" {
-#   type        = string
-#   default     = "200-399"
-# }
+ variable "frontend_health_check_matcher" {
+   type        = string
+   default     = "200-399"
+ }
 
-# variable "backend_health_check_path" {
-#   type        = string
-#   default     = "/health"
-# }
+ variable "backend_health_check_path" {
+   type        = string
+   default     = "/health"
+ }
 
-# variable "backend_health_check_matcher" {
-#   type        = string
-#   default     = "200-399"
-# }
+ variable "backend_health_check_matcher" {
+   type        = string
+   default     = "200-399"
+ }
 
-# variable "health_check_interval" {
-#   type        = number
-#   default     = 30
-# }
+ variable "health_check_interval" {
+   type        = number
+   default     = 30
+ }
 
-# variable "health_check_timeout" {
-#   type        = number
-#   default     = 5
-# }
+ variable "health_check_timeout" {
+   type        = number
+   default     = 5
+ }
 
 # variable "healthy_threshold" {
 #   type        = number
