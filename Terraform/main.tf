845e8860ce98a86b0b32764f0172adcea12555fa
Lokeshwar R <lokeshwarrajan4@gmail.com>
2025-08-19 00:27:26 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -530,26 +530,12 @@ module "aws_sns_topic_euw1" {
   owner                       = var.owner
   
 }
-resource "aws_iam_policy" "ecs_task_secrets_policy" {
-  name        = "ecs_task_role-secrets-policy"
-  description = "Allow ECS task to access specific secrets"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = ["secretsmanager:GetSecretValue"]
-        Resource = module.rds_db_secret.secret_arn
-      }
-    ]
-  })
-}
 module "ecs_task_role" {
   source               = "./Modules/IAM"
   role_name            = var.ecs_task_role_name
   trusted_services     = var.ecs_task_trusted_services
-  policy_arns          = [aws_iam_policy.ecs_task_secrets_policy.arn]
+  policy_arns          = var.ecs_task_policy_arns
   create_instance_profile = false
 }
 module "rds_role" {
